0ef8527c6d86ddda74912ad8eeb35e12d7515874
gyrevik <alex.smotritsky@live.com>
2018-12-15 15:27:18 -0500

    starting to shape the table

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -9,34 +9,39 @@ class ResponsiveTable extends React.Component {
     this.state = {apiID: '814e19f3074443de6b0f91f176dc1026',
                   apiBase: 'http://api.openweathermap.org/data/2.5/forecast',
                   city: 'München',
-                  country: 'DE'
+                  country: 'DE',
+                  type: 'like'
     }
   }
 
   componentDidMount() {
-    const { apiID, apiBase, city, country } = this.state;
-    const url = `${apiBase}?q=${city},${country}&appid=${apiID}`;
+    const { apiID, apiBase, city, country, type } = this.state;
+    const url = `${apiBase}?q=${city},${country}&type=${type}&appid=${apiID}`;
     console.log('url: ', url)
 
     axios.get(url)
       .then(function (response) {
-        console.log(response);
+        console.log('response: ', response);
+        console.log('response.data.list: ', response.data.list)
       })
       .catch(function (error) {
         console.log(error);
       });
   }
 
-  render () {  
+  render () {
+    const indexes = [4, 12, 20, 28, 36]
     return (
       <table>
         <caption>Edgewater, NJ 5 Day Weather</caption>
         <thead>
           <tr>
-            <th scope="col">Account</th>
-            <th scope="col">Due Date</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Period</th>
+            <th scope="col">DAY</th>
+            <th scope="col">DESCRIPTION</th>
+            <th scope="col">HIGH / LOW</th>
+            <th scope="col">PRECIP</th>
+            <th scope="col">WIND</th>
+            <th scope="col">HUMIDITY</th>
           </tr>
         </thead>
         <tbody>
@@ -45,11 +50,15 @@ class ResponsiveTable extends React.Component {
             <td data-label="Due Date">04/01/2016</td>
             <td data-label="Amount">$1,190</td>
             <td data-label="Period">03/01/2016 - 03/31/2016</td>
+            <td data-label="Period">03/01/2016 - 03/31/2016</td>
+            <td data-label="Period">03/01/2016 - 03/31/2016</td>
           </tr>
           <tr>
             <td scope="row" data-label="Account">Visa - 6076</td>
             <td data-label="Due Date">03/01/2016</td>
             <td data-label="Amount">$2,443</td>
+            <td data-label="Period">02/01/2016 - 02/29/2016</td>
+            <td data-label="Period">02/01/2016 - 02/29/2016</td>
             <td data-label="Period">02/01/2016 - 02/29/2016</td>
           </tr>
           <tr>
@@ -57,11 +66,15 @@ class ResponsiveTable extends React.Component {
             <td data-label="Due Date">03/01/2016</td>
             <td data-label="Amount">$1,181</td>
             <td data-label="Period">02/01/2016 - 02/29/2016</td>
+            <td data-label="Period">02/01/2016 - 02/29/2016</td>
+            <td data-label="Period">02/01/2016 - 02/29/2016</td>
           </tr>
           <tr>
             <td scope="row" data-label="Acount">Visa - 3412</td>
             <td data-label="Due Date">02/01/2016</td>
             <td data-label="Amount">$842</td>
+            <td data-label="Period">01/01/2016 - 01/31/2016</td>
+            <td data-label="Period">01/01/2016 - 01/31/2016</td>
             <td data-label="Period">01/01/2016 - 01/31/2016</td>
           </tr>
         </tbody>
