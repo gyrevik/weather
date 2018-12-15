a719161230c7104a4add2ff2d142aa1799588e68
gyrevik <alex.smotritsky@live.com>
2018-12-15 15:59:21 -0500

    removing static data

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -55,46 +55,14 @@ class ResponsiveTable extends React.Component {
         <tbody>
           {list.map((row, key) => 
             <tr key={key}>
-              <td>test</td>
-              <td>test</td>
-              <td>test</td>
-              <td>test</td>
-              <td>test</td>
-              <td>test</td>
+              <td data-label="DAY">test</td>
+              <td data-label="DESCRIPTION">test</td>
+              <td data-label="HIGH / LOW">test</td>
+              <td data-label="PRECIP">test</td>
+              <td data-label="WIND">test</td>
+              <td data-label="HUMIDITY">test</td>
             </tr>
           )}
-          <tr>
-            <td data-label="Account">Visa - 3412</td>
-            <td data-label="Due Date">04/01/2016</td>
-            <td data-label="Amount">$1,190</td>
-            <td data-label="Period">03/01/2016 - 03/31/2016</td>
-            <td data-label="Period">03/01/2016 - 03/31/2016</td>
-            <td data-label="Period">03/01/2016 - 03/31/2016</td>
-          </tr>
-          <tr>
-            <td scope="row" data-label="Account">Visa - 6076</td>
-            <td data-label="Due Date">03/01/2016</td>
-            <td data-label="Amount">$2,443</td>
-            <td data-label="Period">02/01/2016 - 02/29/2016</td>
-            <td data-label="Period">02/01/2016 - 02/29/2016</td>
-            <td data-label="Period">02/01/2016 - 02/29/2016</td>
-          </tr>
-          <tr>
-            <td scope="row" data-label="Account">Corporate AMEX</td>
-            <td data-label="Due Date">03/01/2016</td>
-            <td data-label="Amount">$1,181</td>
-            <td data-label="Period">02/01/2016 - 02/29/2016</td>
-            <td data-label="Period">02/01/2016 - 02/29/2016</td>
-            <td data-label="Period">02/01/2016 - 02/29/2016</td>
-          </tr>
-          <tr>
-            <td scope="row" data-label="Acount">Visa - 3412</td>
-            <td data-label="Due Date">02/01/2016</td>
-            <td data-label="Amount">$842</td>
-            <td data-label="Period">01/01/2016 - 01/31/2016</td>
-            <td data-label="Period">01/01/2016 - 01/31/2016</td>
-            <td data-label="Period">01/01/2016 - 01/31/2016</td>
-          </tr>
         </tbody>
       </table>
     )
